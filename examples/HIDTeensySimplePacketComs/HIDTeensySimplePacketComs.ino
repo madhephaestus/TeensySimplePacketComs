d5ce7961806d24bc04595f7e9ed2e12f875a1ba1
Kevin Harrington <mad.hephaestus@gmail.com>
2018-05-22 17:27:35 -0400

    Updating example

--- examples/HIDTeensySimplePacketComs/HIDTeensySimplePacketComs.ino
+++ examples/HIDTeensySimplePacketComs/HIDTeensySimplePacketComs.ino
@@ -13,11 +13,11 @@
 // NOTE you need to add -lsupc++_s -lstdc++_s to "append to link" compile options
 #include <TeensySimplePacketComs.h>
 
-class GetStatus: public PacketEventAbstract {
+class SetServos: public PacketEventAbstract {
 public:
 	// Packet ID needs to be set
-	GetStatus() :
-			PacketEventAbstract(1964)	// Address of this event
+	SetServos() :
+			PacketEventAbstract(1962)	// Address of this event
 	{
 	}
 	//User function to be called when a packet comes in
@@ -27,14 +27,33 @@ public:
 		uint8_t * bytes = (uint8_t *) buffer;
 		int numberOfBytes = 60;
 		for (int i = 1; i < numberOfBytes; i++) {
-			bytes[i] = 0;	// write a value to the controller
+			///bytes[i] = 0;	// write a value to the controller
+		}
+	}
+};
+
+class GetIMU: public PacketEventAbstract {
+public:
+	// Packet ID needs to be set
+	GetIMU() :
+			PacketEventAbstract(1804)	// Address of this event
+	{
+	}
+	//User function to be called when a packet comes in
+	// Buffer contains data from the packet coming in at the start of the function
+	// User data is written into the buffer to send it back
+	void event(float * buffer) {
+		int numberOfBytes = 15;
+		for (int i = 0; i < numberOfBytes; i++) {
+			buffer[i]=0;
 		}
 	}
 };
 HIDSimplePacket coms;
 
 void setup() {
-	coms.attach(new GetStatus());
+	coms.attach(new SetServos());
+	coms.attach(new GetIMU());
 
 }
 
