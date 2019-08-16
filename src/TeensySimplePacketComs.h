c8fd237ade6121b0e2a041978a9b196e7c9ae17e
Kevin Harrington <mad.hephaestus@gmail.com>
2019-08-16 16:16:44 -0400

    Update TeensySimplePacketComs.h

--- src/TeensySimplePacketComs.h
+++ src/TeensySimplePacketComs.h
@@ -1,9 +1,11 @@
 #if !defined(TEENSYSIMPLEPACKET)
 #define TEENSYSIMPLEPACKET
+/*
 extern "C"{
   int _getpid(){ return -1;}
   int _kill(int pid, int sig){ return -1; }
 }
+*/
 #include "HID/HIDSimplePacket.h"
 
 #endif
